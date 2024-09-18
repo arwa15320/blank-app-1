92578eb4b1ce2905510b4b8dd24ca242eaf9e558
arwa abdelazim <97008135+arwa15320@users.noreply.github.com>
2024-09-18 20:45:00 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,29 @@
+'import time
 import streamlit as st
+area=None
+st.header("Claculate Area")
+with st.sidebar:
+    shape=st.selectbox("choose the shap",["circle","rectangle"])
+if shape=='circle':
+    r=st.number_input("enter your radius",min_value=1,max_value=100)
+    area=r*r*3.14
 
-import streamlit as st
-st.text_input("Enter your name")
-btn=st.button("show")
+elif shape=='rectangle':
+    w=st.number_input("enter your width",min_value=1,max_value=100)
+    h=st.number_input("enter your height",min_value=1,max_value=100)
+    area=w*h             
+
+btn=st.button('calculate')
 if btn:
-  st.write(f'Hello {name}')
+    with st.spinner('loading....'):
+        time.sleep(2)
+    
+    st.write(f"the Area= {area}")
+    st.sidebar.title('sidebar')'''
+import streamlit as st
+import pandas as pd
+st.header('file upload app 2')
+file =st.file_uploader('upload dataset ',type=['csv'])
+if file is not None:
+    df=pd.read_csv(file)
+    st.write(df)
